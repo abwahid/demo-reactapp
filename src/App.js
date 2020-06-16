a587c20fe48a77fb6bdda5722f44b9fe514464fb
Abdul Wahid <itswahid@gmail.com>
2020-06-16 22:49:48 +0500

    demo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Learning from './camp.js';
+import './camp.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( <div className="App">
+            <h1>Hello Wahid</h1>
+            <Learning regards="Enjoy Life" change="climate"></Learning>
+            
+          </div>
+
+  )
 }
 
 export default App;
---
+++ src/camp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Learning(props){
+    return (
+        <dive>
+            <h2>Good Evening Master, {props.regards}</h2>
+            <h3>Stay Home, Stay Save</h3>
+            <p>Now a days weather is very hot, may be it is due to <strong>{props.change}</strong> so take a lot of care of yourself. Due to this change peoples are getting ill, typhoid and savere fever is common.During <l><b>COVID-19</b></l> pandamic people are very scary they are arrested in there houses, bussinesses are effected badly, they are not earning there daily wages. It is said public may not get effected or dei by CORONA but the can dei by starvation. </p>
+        </dive>
+    )
+}
+
+export default Learning;
